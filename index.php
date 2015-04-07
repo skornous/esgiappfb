4e8801838fab067ba500bccea30cc8d299f27618
skornous <hugo.gennaro@gmail.com>
2015-04-07 16:13:16 +0200

    the session is now usefull ! when connected the connect button disapear..

--- index.php
+++ index.php
@@ -14,44 +14,59 @@
 
     FacebookSession::setDefaultApplication(APPID, APPSECRET);
 
-	$helper = new FacebookRedirectLoginHelper("https://esgiappfb.herokuapp.com/");
-	$loginUrl = $helper->getLoginUrl();
+	// If var session exists && $_SESSION['fb_token'] exists -> create user from fb session
+	if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
+		$session = new FacebookSession($_SESSION['fb_token']);
+	} else { // else print connexion's link
+		$session = $helper->getSessionFromRedirect();
+	}
 
 ?>
 
 <!DOCTYPE html>
 <html>
+<body>
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '<?php echo APPID; ?>',
+			xfbml      : true,
+			version    : 'v2.3'
+		});
+	};
+
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
+<h1>App FB ESGI</h1>
+
+<div
+	class="fb-like"
+	data-share="true"
+	data-width="450"
+	data-show-faces="true">
+</div>
+<br>
+
+    <pre>
+	    <?php
+		    if($session){
+		        $_SESSION['fb_token'] = (string) $session->getAccessToken();
+		    } else {
+			    $helper = new FacebookRedirectLoginHelper("https://esgiappfb.herokuapp.com/");
+			    $loginUrl = $helper->getLoginUrl();
+			    echo "<a href=" . $loginUrl . ">Connect with Facebook</a>";
+		    }
+	    ?>
+    </pre>
+</body>
 <head>
 	<meta charset="utf-8">
 	<title>Page d'accueil</title>
 </head>
-<body>
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId      : '<?php echo APPID; ?>',
-                xfbml      : true,
-                version    : 'v2.3'
-            });
-        };
-
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-	<h1>App FB ESGI</h1>
-
-    <div
-        class="fb-like"
-        data-share="true"
-        data-width="450"
-        data-show-faces="true">
-    </div>
-	<br />
-	<a href="<?php echo $loginUrl;?>">Connect with Facebook</a>
-</body>
 </html>
